cdaee778a3cd97d6f855e2944b902d27908e7e04
jenny han <jlhan@stanford.edu>
2021-06-22 10:06:13 -0700

    add giant crayon

--- 08_giantcrayon/sketch.js
+++ 08_giantcrayon/sketch.js
@@ -1,9 +1,13 @@
-
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    //add code!
-}
-
-function draw() {
-    //add code!
-}
+  }
+  
+  function draw() {
+    noStroke();
+    if (mouseIsPressed) {
+      fill('rgba(255, 255, 255, 1)');
+    } else {
+      fill('rgba(144, 212, 184, 1)');
+    }
+    ellipse(mouseX, mouseY, 500, 500);
+  }
